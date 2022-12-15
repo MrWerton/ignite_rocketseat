662c17a3b3b650cc11c62eaa48b55291c97ceeae
werton <mr.werton@gmail.com>
2022-12-14 23:45:45 -0300

    [feat] - created add account method

--- node/module_one/first_api/src/server.js
+++ node/module_one/first_api/src/server.js
@@ -1,7 +1,21 @@
 const express = require('express')
-
+const {v4: UUIDV4} = require('uuid')
 
 const app = express();
+app.use(express.json())
+const db = [];
 
+app.post('/account', (request, response) => {
+    const {cpf, name} = request.body;
+    const id = UUIDV4()
+    const account = {
+        id,
+        cpf,
+        name,
+        statement: []
+    }
+    db.push(account)
+    return response.send(201).send()
+})
 const PORT = 4000;
 app.listen(PORT, () => console.log(`Server run`))
