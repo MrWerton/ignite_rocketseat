ad04747e63691eba3460f8b514fe93087d2f434e
werton <mr.werton@gmail.com>
2022-12-15 20:56:02 -0300

    [feat] - added list category route

--- node/rentx/src/routes/categories.routes.ts
+++ node/rentx/src/routes/categories.routes.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
 
 import { createCategoryController } from "../modules/cars/usecases/create_category";
+import { listCategoriesController } from "../modules/cars/usecases/list_category";
 
 const categoriesRoutes = Router();
 
 categoriesRoutes.post("/", (request, response) => {
     return createCategoryController.handle(request, response);
+});
+
+categoriesRoutes.get("/", (request, response) => {
+    return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
