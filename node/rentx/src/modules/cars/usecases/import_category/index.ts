49d5368b1fe2b9a1743bafd94a6c3b3d4068026a
werton <mr.werton@gmail.com>
2022-12-15 21:06:52 -0300

    [feat] - added import category usecase

---
+++ node/rentx/src/modules/cars/usecases/import_category/index.ts
@@ -0,0 +1,11 @@
+import { CategoriesRepository } from "../../repositories/category/category_repository";
+import { ImportCategoriesController } from "./import_category_controller";
+import { ImportCategoriesUseCase } from "./import_category_usecase";
+
+
+const categoriesRepository = CategoriesRepository.getInstance();
+const importCategoriesUseCase = new ImportCategoriesUseCase(categoriesRepository)
+const importCategoriesController = new ImportCategoriesController(importCategoriesUseCase);
+
+export { importCategoriesController };
+
