dd6747ea0a31e17f63bd03416082dde80d60abe8
werton <mr.werton@gmail.com>
2022-12-16 12:54:04 -0300

    [feat] - added documentation

--- node/rentx/src/server.ts
+++ node/rentx/src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from '../swagger.json';
 import { categoriesRoutes } from "./routes/categories.routes";
 import { specificationRoutes } from "./routes/specification.routes";
-
 const app = express();
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use('/categories', categoriesRoutes)
 app.use('/specification', specificationRoutes)
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server run in http://localhost/${PORT}`));
+app.listen(PORT, () => console.log(`Server run in http://localhost:${PORT}`));
