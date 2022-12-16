2f4228a7a30f569ac7a37ba50253dfd5f961ada4
Werton <mr.werton@gmail.com>
2022-12-16 16:43:52 -0300

    [feat] -- configured docker

--- node/rentx/src/server.ts
+++ node/rentx/src/server.ts
@@ -6,8 +6,11 @@ import { categoriesRoutes } from "./routes/categories.routes";
 import { specificationRoutes } from "./routes/specification.routes";
 
 const app = express();
+
+app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/categories", categoriesRoutes);
 app.use("/specification", specificationRoutes);
-const PORT = process.env.PORT || 4000;
+
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Server run in http://localhost:${PORT}`));
