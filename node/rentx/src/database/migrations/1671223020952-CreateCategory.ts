b2764b57baa1faa917ae87bcac751bdee587bf2d
Werton <mr.werton@gmail.com>
2022-12-16 17:37:39 -0300

    [feat] - configured cli of typeorm for generate migrations

---
+++ node/rentx/src/database/migrations/1671223020952-CreateCategory.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateCategory1671223020952 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
